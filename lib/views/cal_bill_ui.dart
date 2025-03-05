4b977a2bc7af56609ecdae8feef012dfcaa402a5
chalermsukchanintorn <chalermsukchanintorn@gmail.com>
2025-03-05 12:08:19 +0700

    splsh เสร็จแล้ว menu เสร็จแล้ว

---
+++ lib/views/cal_bill_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CallBillUI extends StatefulWidget {
+  const CallBillUI({super.key});
+
+  @override
+  State<CallBillUI> createState() => _CallBillUIState();
+}
+
+class _CallBillUIState extends State<CallBillUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+    );
+  }
+}
